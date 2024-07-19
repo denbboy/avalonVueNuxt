6d17e1b5a7a41cee0db961c5fea0a546609f1b6e
Vlas <vlas.zub@gmail.com>
2024-07-19 13:32:44 +0300

    Fix grid columns ni projects

--- components/pages/main/Projects.vue
+++ components/pages/main/Projects.vue
@@ -96,3 +96,19 @@ const getGridClasses = (index) => {
 };
 
 </script>
+
+
+<style scoped>
+    .col-start-2 {
+        grid-column-start: 2;
+    }
+    .col-start-3 {
+        grid-column-start: 3;
+    }
+    .col-start-4 {
+        grid-column-start: 4;
+    }
+    .col-end-4 {
+        grid-column-end: 4;
+    }
+</style>
