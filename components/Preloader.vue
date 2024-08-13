f6786f58677a40d9afb8b6e95f70362e97f75497
Vlas <vlas.zub@gmail.com>
2024-08-13 19:51:14 +0300

    change preloader

--- components/Preloader.vue
+++ components/Preloader.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="fixed left-0 top-0 h-full w-full z-50 bg-blue-500 transition-all duration-150 flex items-center justify-center"
         :class="isLoading ? 'visible opacity-100' : 'opacity-0 invisible'">
-        <iframe v-if="isLoading" class="w-[800px] h-[800px]"
-            src="https://lottie.host/embed/1b26babb-a87c-48e7-9a0a-f31a06298110/CixbB89Yfp.json"></iframe>
+        <!-- <iframe v-if="isLoading" class="w-[800px] h-[800px]"
+            src="https://lottie.host/embed/1b26babb-a87c-48e7-9a0a-f31a06298110/CixbB89Yfp.json"></iframe> -->
+
+            <img src="/assets/img/loader.gif" class="w-[800px] h-[500px]" alt="">
     </div>
 </template>
 
