d7985f8d0f3a2d44a2f25e7444a0308da89d5e3b
Vlas <vlas.zub@gmail.com>
2025-05-01 10:33:31 +0300

    Error Fixed Image

--- error.vue
+++ error.vue
@@ -7,7 +7,7 @@
 
     <div class="container">
 
-      <NuxtImg src="/img/404.png" loading="lazy" class="mx-auto" format="png" alt="Not found" width="960" />
+      <img src="/img/404.png" loading="lazy" class="mx-auto" format="png" alt="Not found" width="960" />
 
       <p class="text-white md:text-lg mt-7 md:mt-12 text-center mx-auto max-w-4xl">
         {{  $t('404_text')  }}
@@ -17,10 +17,10 @@
       </NuxtLink>
 
       <div class="absolute -right-[38px] top-[200px] w-[76px] z-10 md:w-[280px] md:-right-[140px]">
-        <NuxtImg loading="lazy" src="/img/icons/vector-logo.svg" class="w-full" alt="vector-logo" />
+        <img loading="lazy" src="/img/icons/vector-logo.svg" class="w-full" alt="vector-logo" />
       </div>
       <div class="absolute -left-[38px] bottom-[200px] w-[76px] z-10 md:w-[280px] md:-left-[140px]">
-        <NuxtImg loading="lazy" src="/img/icons/vector-logo.svg" class="w-full" alt="vector-logo" />
+        <img loading="lazy" src="/img/icons/vector-logo.svg" class="w-full" alt="vector-logo" />
       </div>
 
     </div>
