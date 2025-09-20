dff34e0ae9e2be0ed6d003edc6cdd1000944d59b
AlexV0102 <oleksandr.vasyliev04@gmail.com>
2025-09-20 23:17:27 +0300

    modal mobile rework, add new modal version

--- components/modals/Modal.vue
+++ components/modals/Modal.vue
@@ -2,21 +2,53 @@
   <Transition>
     <div v-if="modals[0]" class="z-50 relative">
       <div
-        class="transition-all modal fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center modal-special modal">
-        <div class="modal__bgd absolute top-0 left-0 bg-[#031523]/90 w-full h-full z-0" @click="closeModal">
-        </div>
+        class="transition-all modal fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center modal-special modal"
+      >
         <div
-          class="modal__content relative z-10 bg-blue-500 md:rounded-[20px] h-full md:h-auto flex items-center justify-center flex-col">
-          <div class="modal__cross modal__close absolute top-2 right-2 md:-top-10 z-[1] md:-right-10 cursor-pointer group"
-            @click="closeModal">
-            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect class="hidden md:block group-hover:opacity-80 transition-all" opacity="0.2" x="0.5" y="0.5" width="29" height="29" rx="9.5"
-                stroke="white" />
+          class="modal__bgd absolute top-0 left-0 bg-[#031523]/90 w-full h-full z-0"
+          @click="closeModal"
+        ></div>
+        <div
+          class="modal__content relative z-10 bg-blue-500 rounded-[20px] h-auto flex items-center justify-center flex-col"
+        >
+          <div
+            class="modal__cross modal__close absolute top-2 right-2 md:-top-10 z-[1] md:-right-10 cursor-pointer group"
+            @click="closeModal"
+          >
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                class="hidden md:block group-hover:opacity-80 transition-all"
+                opacity="0.2"
+                x="0.5"
+                y="0.5"
+                width="29"
+                height="29"
+                rx="9.5"
+                stroke="white"
+              />
               <g class="opacity-100 group-hover:opacity-80 transition-all md:opacity-50">
-                <rect x="8.05469" y="7" width="20.9207" height="1.49433" transform="rotate(45 8.05469 7)"
-                  fill="white" />
-                <rect x="7" y="21.9434" width="20.9207" height="1.49433" transform="rotate(-45 7 21.9434)"
-                  fill="white" />
+                <rect
+                  x="8.05469"
+                  y="7"
+                  width="20.9207"
+                  height="1.49433"
+                  transform="rotate(45 8.05469 7)"
+                  fill="white"
+                />
+                <rect
+                  x="7"
+                  y="21.9434"
+                  width="20.9207"
+                  height="1.49433"
+                  transform="rotate(-45 7 21.9434)"
+                  fill="white"
+                />
               </g>
             </svg>
           </div>
@@ -27,7 +59,7 @@
           <ModalsModalMessage v-else-if="modals[0] === 'message'" />
           <ModalsModalVacancy v-else-if="modals[0] === 'vacancy'" />
           <ModalsModalRtsp v-else-if="modals[0] === 'rtsp'" />
-
+          <ModalsModalPromo v-else-if="modals[0] === 'promo'" />
         </div>
       </div>
     </div>
@@ -35,21 +67,53 @@
   <Transition>
     <div v-if="modals[1]" class="z-50 relative">
       <div
-        class="transition-all modal fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center modal-special modal">
-        <div class="modal__bgd absolute top-0 left-0 bg-[#031523]/90 w-full h-full z-0" @click="closeModal">
-        </div>
+        class="transition-all modal fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center modal-special modal"
+      >
         <div
-          class="modal__content relative z-10 bg-blue-500 md:rounded-[20px] h-full md:h-auto flex items-center justify-center flex-col">
-          <div class="modal__cross modal__close absolute top-2 right-2 md:-top-10 z-[1] md:-right-10 cursor-pointer"
-            @click="closeModal">
-            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect class="hidden md:block" opacity="0.2" x="0.5" y="0.5" width="29" height="29" rx="9.5"
-                stroke="white" />
+          class="modal__bgd absolute top-0 left-0 bg-[#031523]/90 w-full h-full z-0"
+          @click="closeModal"
+        ></div>
+        <div
+          class="modal__content relative z-10 bg-blue-500 rounded-[20px] h-auto flex items-center justify-center flex-col"
+        >
+          <div
+            class="modal__cross modal__close absolute top-2 right-2 md:-top-10 z-[1] md:-right-10 cursor-pointer"
+            @click="closeModal"
+          >
+            <svg
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                class="hidden md:block"
+                opacity="0.2"
+                x="0.5"
+                y="0.5"
+                width="29"
+                height="29"
+                rx="9.5"
+                stroke="white"
+              />
               <g class="opacity-100 md:opacity-50">
-                <rect x="8.05469" y="7" width="20.9207" height="1.49433" transform="rotate(45 8.05469 7)"
-                  fill="white" />
-                <rect x="7" y="21.9434" width="20.9207" height="1.49433" transform="rotate(-45 7 21.9434)"
-                  fill="white" />
+                <rect
+                  x="8.05469"
+                  y="7"
+                  width="20.9207"
+                  height="1.49433"
+                  transform="rotate(45 8.05469 7)"
+                  fill="white"
+                />
+                <rect
+                  x="7"
+                  y="21.9434"
+                  width="20.9207"
+                  height="1.49433"
+                  transform="rotate(-45 7 21.9434)"
+                  fill="white"
+                />
               </g>
             </svg>
           </div>
@@ -60,7 +124,7 @@
           <ModalsModalMessage v-else-if="modals[1] === 'message'" />
           <ModalsModalVacancy v-else-if="modals[1] === 'vacancy'" />
           <ModalsModalRtsp v-else-if="modals[1] === 'rtsp'" />
-
+          <ModalsModalPromo v-else-if="modals[1] === 'promo'" />
         </div>
       </div>
     </div>
@@ -97,7 +161,7 @@ watch(modals, (newModals) => {
 
 // Close Modal Function
 const closeModal = () => {
-  modalsStore.removeModalData()
+  modalsStore.removeModalData();
   modalsStore.removeModal(modals.value[modals.value.length - 1]);
 };
 </script>
--- components/modals/ModalMessage.vue
+++ components/modals/ModalMessage.vue
@@ -1,205 +1,203 @@
 <template>
-  <div
-    class="content__inner w-full z-0 relative overflow-hidden p-5 lg:p-10 max-w-[600px] flex flex-col items-center h-full"
-  >
-    <h2 class="text-white text-lg md:text-2xl font-bold text-center mb-5">
-      {{ $t('m_message_text_1') }}
-    </h2>
+    <div
+      class="content__inner w-full z-0 relative overflow-hidden p-5 lg:p-10 max-w-[600px] flex flex-col items-center h-full"
+    >
+      <h2 class="text-white text-lg md:text-2xl font-bold text-center mb-5">
+        {{ $t('m_message_text_1') }}
+      </h2>
 
-    <form @submit.prevent="submitForm" class="flex w-full flex-col text-center">
-      <div class="flex flex-col">
-        <label for="phone-modal" class="flex text-white text-xs mb-[10px]">
-          {{ $t('m_message_text_2') }}
-        </label>
-        <div class="phone-vti">
-          <VueTelInput
-            :input-options="inputOptions"
-            v-model="phone"
-            :preferred-countries="preferredCountries"
-            :only-countries="sortedCountries"
-            @input="inputPhoneNumber"
-          />
-        </div>
+      <form @submit.prevent="submitForm" class="flex w-full flex-col text-center">
+        <div class="flex flex-col">
 
-        <p
-          class="text-red-700 text-left transition-all h-full"
-          :class="{
-            'max-h-10 opacity-100 mt-2': isError,
-            'max-h-0 opacity-0': !isError,
-          }"
-        >
-          {{ $t('fill_all_fields') }}
-        </p>
-        <p
-          class="text-white/50 text-left transition-all h-full"
-          :class="{
-            'max-h-10 opacity-100 mt-2': isSending,
-            'max-h-0 opacity-0': !isSending,
-          }"
-        >
-          {{ $t('wait_for_send') }}
-        </p>
-        <p
-          class="text-green-500 text-left transition-all h-full"
-          :class="{
-            'max-h-10 opacity-100 mt-2': isSuccess,
-            'max-h-0 opacity-0': !isSuccess,
-          }"
-        >
-          {{ $t('message_sent_successfuly') }}
-        </p>
+          <div class="phone-vti">
+            <VueTelInput
+              :input-options="inputOptions"
+              v-model="phone"
+              :preferred-countries="preferredCountries"
+              :only-countries="sortedCountries"
+              @input="inputPhoneNumber"
+            />
+          </div>
 
-        <button
-          type="submit"
-          class="white-button w-full mt-5 lg:leading-[120%] lg:py-5 leading-[90%]"
-        >
-          {{ $t('send') }}
-        </button>
-
-        <p class="text-center text-white text-xs lg:text-sm w-full mt-5">
-          {{ $t('m_message_text_3') }}
-          <NuxtLink
-            href="/docs/privacy-police"
-            class="text-blue-400 font-bold underline transition-all hover:text-blue-700"
+          <p
+            class="text-red-700 text-left transition-all h-full"
+            :class="{
+              'max-h-10 opacity-100 mt-2': isError,
+              'max-h-0 opacity-0': !isError,
+            }"
           >
-            {{ $t('policy') }}
-          </NuxtLink>
-        </p>
-      </div>
-    </form>
-  </div>
-</template>
+            {{ $t('fill_all_fields') }}
+          </p>
+          <p
+            class="text-white/50 text-left transition-all h-full"
+            :class="{
+              'max-h-10 opacity-100 mt-2': isSending,
+              'max-h-0 opacity-0': !isSending,
+            }"
+          >
+            {{ $t('wait_for_send') }}
+          </p>
+          <p
+            class="text-green-500 text-left transition-all h-full"
+            :class="{
+              'max-h-10 opacity-100 mt-2': isSuccess,
+              'max-h-0 opacity-0': !isSuccess,
+            }"
+          >
+            {{ $t('message_sent_successfuly') }}
+          </p>
 
-<script setup>
-import { ref, watch } from 'vue';
-import { usePagesStore } from '~/stores/functions/pages';
-import { useToolkit } from '~/stores/functions/toolkit';
-import { VueTelInput } from 'vue-tel-input';
-import 'vue-tel-input/vue-tel-input.css';
-import iso31661 from 'iso-3166-1';
-import { useReCaptcha } from 'vue-recaptcha-v3';
-import { useFormsStore } from '~/stores/functions/forms';
+          <button
+            type="submit"
+            class="white-button w-full mt-5 lg:leading-[120%] lg:py-5 leading-[90%]"
+          >
+            {{ $t('send') }}
+          </button>
 
-const recaptchaInstance = useReCaptcha();
+          <p class="text-center text-white text-xs lg:text-sm w-full mt-5">
+            {{ $t('m_message_text_3') }}
+            <NuxtLink
+              href="/docs/privacy-police"
+              class="text-blue-400 font-bold underline transition-all hover:text-blue-700"
+            >
+              {{ $t('policy') }}
+            </NuxtLink>
+          </p>
+        </div>
+      </form>
+    </div>
+  </template>
 
-const recaptcha = async () => {
-  await recaptchaInstance?.recaptchaLoaded();
-  const token = await recaptchaInstance?.executeRecaptcha('yourActionHere');
-  return token;
-};
+  <script setup>
+  import { ref, watch } from 'vue';
+  import { usePagesStore } from '~/stores/functions/pages';
+  import { useToolkit } from '~/stores/functions/toolkit';
+  import { VueTelInput } from 'vue-tel-input';
+  import 'vue-tel-input/vue-tel-input.css';
+  import iso31661 from 'iso-3166-1';
+  import { useReCaptcha } from 'vue-recaptcha-v3';
+  import { useFormsStore } from '~/stores/functions/forms';
 
-const pagesStore = usePagesStore();
-const langStore = useLangStore();
-const formsStore = useFormsStore();
-const allPages = ref([]);
+  const recaptchaInstance = useReCaptcha();
 
-const currentForm =
-  formsStore.forms.length && formsStore.forms?.filter((item) => item.slug === 'offer-form')[0];
+  const recaptcha = async () => {
+    await recaptchaInstance?.recaptchaLoaded();
+    const token = await recaptchaInstance?.executeRecaptcha('yourActionHere');
+    return token;
+  };
 
-watch(pagesStore, (newValue) => {
-  allPages.value = newValue?.pagesList;
-});
+  const pagesStore = usePagesStore();
+  const langStore = useLangStore();
+  const formsStore = useFormsStore();
+  const allPages = ref([]);
 
-const phone = ref(null);
-const isError = ref(null);
-const errorText = ref('');
-const isSending = ref(false);
-const isSuccess = ref(false);
+  const currentForm =
+    formsStore.forms.length && formsStore.forms?.filter((item) => item.slug === 'offer-form')[0];
 
-const inputPhoneNumber = () => {
-  phone.value = phone.value.replace(/(?!^\+)[^\d]/g, '');
-};
+  watch(pagesStore, (newValue) => {
+    allPages.value = newValue?.pagesList;
+  });
 
-const toolkitStore = useToolkit();
+  const phone = ref(null);
+  const isError = ref(null);
+  const errorText = ref('');
+  const isSending = ref(false);
+  const isSuccess = ref(false);
 
-const resetForm = () => {
-  phone.value = '';
-};
+  const inputPhoneNumber = () => {
+    phone.value = phone.value.replace(/(?!^\+)[^\d]/g, '');
+  };
 
-const placeholderLang = {
-  ru: 'Введите ваш номер телефона',
-  en: 'Enter your phone number',
-  ua: 'Введіть ваш номер телефону',
-};
+  const toolkitStore = useToolkit();
 
-const inputOptions = {
-  showDialCode: true,
-  autoFormat: false,
-  placeholder: placeholderLang[langStore.lang],
-  maxlength: 15,
-};
+  const resetForm = () => {
+    phone.value = '';
+  };
 
-const submitForm = async () => {
-  if (currentForm?.captcha) {
-    if (!recaptcha()) {
-      errorText.value = 'Recaptcha error';
-      return (isError.value = true);
+  const placeholderLang = {
+    ru: 'Введите ваш номер телефона',
+    en: 'Enter your phone number',
+    ua: 'Введіть ваш номер телефону',
+  };
+
+  const inputOptions = {
+    showDialCode: true,
+    autoFormat: false,
+    placeholder: placeholderLang[langStore.lang],
+    maxlength: 15,
+  };
+
+  const submitForm = async () => {
+    if (currentForm?.captcha) {
+      if (!recaptcha()) {
+        errorText.value = 'Recaptcha error';
+        return (isError.value = true);
+      }
     }
-  }
 
-  try {
-    if (!phone.value || phone.value.length < 10) {
-      isError.value = true;
-      return;
-    } else {
-      isError.value = false;
-    }
+    try {
+      if (!phone.value || phone.value.length < 10) {
+        isError.value = true;
+        return;
+      } else {
+        isError.value = false;
+      }
 
-    isSending.value = true;
+      isSending.value = true;
 
-    await useFetch('/api/send-form', {
-      method: 'POST',
-      body: {
-        phone: phone.value,
-        form: 'message',
-      },
-    }).then((res) => {
+      await useFetch('/api/send-form', {
+        method: 'POST',
+        body: {
+          phone: phone.value,
+          form: 'message',
+        },
+      }).then((res) => {
+        isSending.value = false;
+        isSuccess.value = true;
+        resetForm();
+      });
+    } catch (error) {
+      console.error('Contact form could not be sent', error);
       isSending.value = false;
-      isSuccess.value = true;
-      resetForm();
-    });
-  } catch (error) {
-    console.error('Contact form could not be sent', error);
-    isSending.value = false;
-  }
-};
+    }
+  };
 
-const preferredCountries = [
-  'id', // Индонезия
-  'ua', // Украина
-  'ru', // Россия
-  'by', // Беларусь
-  'kz', // Казахстан
-  'us', // США
-  'gb', // Англия
-  'fr', // Франция
-  'cn', // КНР
-];
+  const preferredCountries = [
+    'id', // Индонезия
+    'ua', // Украина
+    'ru', // Россия
+    'by', // Беларусь
+    'kz', // Казахстан
+    'us', // США
+    'gb', // Англия
+    'fr', // Франция
+    'cn', // КНР
+  ];
 
-// Получаем список всех стран
-const allCountries = iso31661.all().map((country) => country.alpha2);
+  // Получаем список всех стран
+  const allCountries = iso31661.all().map((country) => country.alpha2);
 
-// Создаем computed property, который включает все страны
-const sortedCountries = computed(() => {
-  const preferredSet = new Set(preferredCountries);
-  const unselectedCountries = allCountries.filter((country) => !preferredSet.has(country));
-  return [...preferredCountries, ...unselectedCountries];
-});
+  // Создаем computed property, который включает все страны
+  const sortedCountries = computed(() => {
+    const preferredSet = new Set(preferredCountries);
+    const unselectedCountries = allCountries.filter((country) => !preferredSet.has(country));
+    return [...preferredCountries, ...unselectedCountries];
+  });
 
-import { createDirectus, rest, readFlow } from '@directus/sdk';
+  import { createDirectus, rest, readFlow } from '@directus/sdk';
 
-const client = createDirectus('https://api.avalonbali.com').with(rest());
+  const client = createDirectus('https://api.avalonbali.com').with(rest());
 
-const formRequest = async () => {
-  const formData = new FormData();
-  formData.append('phone', phone.value);
+  const formRequest = async () => {
+    const formData = new FormData();
+    formData.append('phone', phone.value);
 
-  const result = await client.request(
-    readFlow('78957f27-5d57-49a2-bdfe-93be73e7e487', {
-      fields: ['*'],
-      body: formData,
-      method: 'POST',
-    }),
-  );
-};
-</script>
+    const result = await client.request(
+      readFlow('78957f27-5d57-49a2-bdfe-93be73e7e487', {
+        fields: ['*'],
+        body: formData,
+        method: 'POST',
+      }),
+    );
+  };
+  </script>
